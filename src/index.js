f098ebeceba7d08c22fd9bb6e4fccfa475ac9bb5
vedadramic <vedad.ramic.23@size.ba>
2026-08-08 19:03:28 +0200

    Stage 1: fetch and cache HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Polite scraper starting...');
+const { fetchPage } = require('./fetcher');
+
+const BASE_URL = 'https://books.toscrape.com/catalogue/page-1.html';
+
+async function main() {
+  const { html } = await fetchPage(BASE_URL);
+  console.log(`Page size: ${html.length} bytes`);
+}
+
+main().catch(console.error);
